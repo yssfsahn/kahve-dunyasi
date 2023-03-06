2840ab3ef72af7843cb4d269e8b84c7f2ebe22a6
Yusuf Ziya <55126376+yssfsahn@users.noreply.github.com>
2023-03-06 17:57:16 +0300

    Kahve Dünyasına hoşgeldiniz

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main {
+void main() {
+  runApp(
+    YusufBox(),
+  );
+}
 
+class YusufBox extends StatelessWidget {
+  const YusufBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.orange[600],
+        body: Center(
+          child: Text(
+            'Bottom',
+            style: TextStyle(fontSize: 60),
+          ),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.pink[800],
+          title: Text('God of Flutter'),
+          centerTitle: true,
+        ),
+      ),
+    );
+  }
 }
